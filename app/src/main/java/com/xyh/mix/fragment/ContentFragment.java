1ceb55a8431e8708a00a24392010d62fad1d97da
Innux <innux_xu@163.com>
2017-03-15 17:04:57 +0800

    1

--- app/src/main/java/com/xyh/mix/fragment/ContentFragment.java
+++ app/src/main/java/com/xyh/mix/fragment/ContentFragment.java
@@ -2,11 +2,14 @@ package com.xyh.mix.fragment;
 
 import android.view.View;
 
+import com.shizhefei.view.indicator.IndicatorViewPager;
+
 /**
  * Created by xyh on 2017/3/13.
  */
 
 public class ContentFragment extends BaseFragment {
+    private IndicatorViewPager mIndicatorViewPager;
 
     @Override
     public void initData() {
